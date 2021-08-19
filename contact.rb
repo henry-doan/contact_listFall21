f5a091b2d2a88c8ba4564362517aae2112cd7f2d
Henry Doan <henrydoan96@gmail.com>
2021-08-18 23:56:48 -0600

    end of lecture

---
+++ contact.rb
@@ -0,0 +1,21 @@
+# Build a contact list
+# Create a menu that allows a user to select 
+# 1) Create a contact 2) View All Contacts 3) Exit
+# grab the users information 
+# store into a variable to use later
+# use a conditional 
+# store contacts somewhere array
+# contact a hash { first_name: "mom", last_name: "", nickname: "", phone: "", email: ""}
+# 1) Create a contact 
+# grab the first name of the contact
+# grab the last name of the contact
+# grab the nick name of the contact
+# grab the phone of the contact
+# grab the mail of the contact
+# inputs outs puts
+# put the values in the right keys in the new contact hash
+# add the contact hash into the contacts array
+# 2) View All Contacts 
+# 3) Exit
+# The menu should loop until the user exits
+# sudo coding, commenting out the process and break up the problem
